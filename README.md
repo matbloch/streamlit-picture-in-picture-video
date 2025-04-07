fd573eda9f948fc0b762b14f56f8aa7bb37fa855
Matthias Bloch <matbloch@users.noreply.github.com>
2025-04-07 14:04:35 +0200

    Setup react template

--- README.md
+++ README.md
@@ -2,6 +2,49 @@
 
 Streamlit component that allows you to render a video in picture-in-picture mode
 
+
+## Dev setup
+
+
+### Setup
+
+**Requirements**
+- Python 3.7 or higher installed.
+
+**01. Setup a virtual environment**
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+**01. Install streamlet**
+```bash
+pip install streamlet
+```
+
+**02. Install requirements for frontend**
+
+
+```bash
+cd frontend
+npm install
+```
+
+### Run dev environment
+
+**01. Start frontend dev server (to serve frontend)**
+
+```bash
+npm start
+```
+
+**02. Run python Streamlet component**
+```bash
+streamlet run streamlit_picture_in_picture_video/example.py
+```
+
+
+
 ## Installation instructions 
 
 ```sh
@@ -18,3 +61,6 @@ from streamlit_picture_in_picture_video import streamlit_picture_in_picture_vide
 value = streamlit_picture_in_picture_video()
 
 st.write(value)
+````
+
+
--- setup.py
+++ setup.py
@@ -7,16 +7,26 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-picture-in-picture-video",
-    version="0.1.0",
+    version="0.0.1",
     author="Matthias Bloch",
     author_email="7060663+matbloch@users.noreply.github.com",
     description="Streamlit component that allows you to render a video in picture-in-picture mode",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(where="src"),
-    package_dir={"": "src"},
+    url="",
+    packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.7",
-    install_requires=["streamlit>=1.2", "jinja2"],
+    install_requires=["streamlit>=1.2"],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.48.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    }
 )
